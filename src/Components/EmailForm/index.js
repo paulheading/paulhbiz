0e2d7dd3629253c6dfbc85fdb36735338087bac3
Paul Heading <paul.heading@barbican.org.uk>
2021-03-07 19:04:38 +0000

    pffff who knows

--- src/Components/EmailForm/index.js
+++ src/Components/EmailForm/index.js
@@ -1,34 +1,42 @@
 import React from "react";
 import { connect } from "react-redux";
-import { useForm } from 'react-hook-form';
-// import Button from "react-bootstrap/Button";
+import Button from "react-bootstrap/Button";
 
 function EmailForm() {
-  const { register, handleSubmit, watch, errors } = useForm();
+  document.querySelector("form").addEventListener("submit", handleSubmit);
 
-  function encode(data) {
-    return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&")
-  }
-
-  const onSubmit = event => {
-    fetch("/", {
-      method: "POST",
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let myForm = document.getElementById('pizzaOrder');
+    let formData = new FormData(myForm)
+    fetch('/', {
+      method: 'POST',
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "hookTest", event })
-    }).then(data => console.log('form data: ', data)).catch(error => alert(error))
+      body: new URLSearchParams(formData).toString()
+    }).then(() => console.log('Form successfully submitted')).catch((error) =>
+      alert(error))
   }
-
-  console.log(watch('example'));
-
+  
   return (
-    <form data-netlify="true" name="contact" method="post" onSubmit={handleSubmit(onSubmit)}>
-      <input type="hidden" name="form-name" value="contact" />
-      <input name="example" defaultValue="test" ref={register} type="text" />
-      <input name="exampleRequired" ref={register({ required: true })} type="text" />
-      {errors.exampleRequired && <span>This field is required</span>}
-      <input type="submit" />
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>   
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Your Role: <select name="role[]" multiple>
+          <option value="leader">Leader</option>
+          <option value="follower">Follower</option>
+        </select></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
     </form>
   );
 }
