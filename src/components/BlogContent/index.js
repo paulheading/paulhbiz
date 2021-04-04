2b073970a62604ae893a9b6e926c9ded3f6cf18c
Paul Heading <paul.heading@barbican.org.uk>
2021-04-04 19:53:14 +0100

    remove old selector

--- src/components/BlogContent/index.js
+++ src/components/BlogContent/index.js
@@ -1,10 +1,8 @@
 import React from "react";
-import { connect, useSelector } from "react-redux";
+import { connect } from "react-redux";
 import { TrelloPage } from "components/Trello";
 
 function BlogContent() {
-  const medium = useSelector((state) => state.mediumData);
-
   return (
     <div className="component-about-content">
       <div className="feed-content__container">
