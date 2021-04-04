8055c3c3b754ee0a43ea82ea377411a43e739b5e
Paul Heading <paul.heading@barbican.org.uk>
2021-04-04 19:43:51 +0100

    roll back

--- src/components/Trello/TrelloPage/index.js
+++ src/components/Trello/TrelloPage/index.js
@@ -4,18 +4,13 @@ import { objectReady, parse } from "modules/helpers";
 function TrelloPage({ name }) {
   const trello = useSelector((state) => state.trelloData);
   const ready = objectReady(trello);
+  let content = "placeholder";
 
-  function printPage() {
-    let content = "placeholder";
-    
-    if (ready) { 
-      trello.pages.cards.map((card) => name === card.name ? content = card.desc : null); 
-    }
-
-    return parse(content);    
-  }
-
-  return printPage();
+  if (ready) { 
+    trello.pages.cards.map((card) => name === card.name ? content = card.desc : null); 
+  }  
+  
+  return parse(content);
 }
 
 const mapStateToProps = (state) => {
