c9344c9a82b4c4dcb3bfec29996e1ded691e174a
Paul Heading <paul.heading@barbican.org.uk>
2021-04-19 11:36:41 +0100

    cool date stuff

--- src/components/ArticleContent/HappyRoute/index.js
+++ src/components/ArticleContent/HappyRoute/index.js
@@ -1,25 +1,52 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Helmet } from 'react-helmet';
+import moment from "moment";
 import { parse, remove, seo, filter } from "modules/helpers";
 
 function ArticleContent({ card }) {
   const live = filter.in.live(card.attachments);
   const code = filter.in.code(card.attachments);
+  const title = remove.hero(card.name);
+
+  const due  = moment(card.due).format("MMM YYYY");
+  const start = moment(card.start).format("MMM YYYY");
+
+  function dateSpan(due, start) {
+    const months = moment(due).diff(start, "months");
+    return months > 1 ? `${months} Months` : `${months} Month`;
+  }
+
+  function dateSummary() {
+    const invalid = "Invalid date";
+    const printFinish = () => card.dueComplete ? <div>Finished: {due}</div> : <div>In progress</div>;
+
+    if (due !== invalid && start !== invalid) {
+      return (
+        <div className="summary feed-content">
+          {printFinish()}
+          <div>Lasted: {dateSpan(due,start)}</div>
+        </div>
+      );
+    }
+  }
   
   return (
     <div className="component-about-content">
       <Helmet>
-        <title>{ seo.title(remove.hero(card.name)) }</title>
+        <title>{ seo.title(title) }</title>
         <meta name="description" content={ card.desc } />
       </Helmet>
       <div className="container feed-content">
         <div className="wrap feed-content">
-          <p>{ remove.hero(card.name) }</p>
+          <h1>{ title }</h1>
+          {dateSummary()}
+          <div className="summary feed-content">
+            { live && <a href={live.url}>See project</a> }
+            { code && <a href={code.url}>See code</a> }
+          </div>
           { parse(card.desc) }          
-          { live && <p>You can see the <a href={live.url}>live project here</a></p> }
-          { code && <p>You can see the <a href={code.url}>live code here</a></p> }
-        </div>
+         </div>
       </div>
     </div>
   );
