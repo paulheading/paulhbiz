86cc2cdab92b440ce8cf0d185bf5571f52585373
Paul Heading <paul.heading@barbican.org.uk>
2021-06-22 11:06:15 +0100

    tweak trello feed

--- src/components/Trello/Cards/Desktop/index.js
+++ src/components/Trello/Cards/Desktop/index.js
@@ -1,24 +1,27 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { filter, print } from "modules/helpers";
+import { remove, filter, print } from "modules/helpers";
 import { Card } from "react-bootstrap";
 
 export default function DesktopCards() {
   const trello = useSelector(state => state.trello);
+  let content = trello.ready ? filter.trello(trello, "Projects") : filter.out.hero(trello.projects.cards); 
+  content = content.filter((_, index) => index < 3);
   const subtitle = <span className="title card-subtitle placeholder">.</span>;
 
-  return filter.out.hero(trello.projects.cards).map((card, index) => {
+  return content.map((card, index) => {
     const placeholder = card.placeholder ? "placeholder" : "";
     const link = filter.in.more(card.attachments);
+    const name = remove.hero(card.name);
     
     return (
       <Card className="trello-desktop" key={`trello-desktop-${index}`}>
         <Link className="link card-name" to={link.url}>
-          <span className={`title card-name ${placeholder}`}>{ filter.string(card.name,40) }</span>
+          <span className={`title card-name ${placeholder}`}>{ filter.string(name,40) }</span>
         </Link>
         { !trello.ready && subtitle }
-        { card.labels && print.labels(card) }
+        { card.labels && print.labels(card.labels) }
       </Card>
     );
   });  
