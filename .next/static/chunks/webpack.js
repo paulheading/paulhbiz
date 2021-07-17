08265b4a5cc7aaff8470ea07f940a150ba30bc67
Paul Heading <paul.heading@barbican.org.uk>
2021-07-17 19:05:56 +0100

    print link

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d8126741cb7e182daede"; }
+/******/ 		__webpack_require__.h = function() { return "30f2730a8229973125f3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
