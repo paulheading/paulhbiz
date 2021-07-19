b890a5af3b4f7aea0410dd932587504ef8fb2698
Paul Heading <paul.heading@barbican.org.uk>
2021-07-19 10:21:56 +0100

    morning changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a939ae6b0e17e9a03fbe"; }
+/******/ 		__webpack_require__.h = function() { return "49c899fb65e6af44605b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
