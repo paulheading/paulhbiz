0518a8e512ee4823db2439d3513e24af4136c67f
Paul Heading <paulheading@me.com>
2021-08-22 16:42:19 +0100

    basic updates

--- .next/server/pages/api/timezone.js
+++ .next/server/pages/api/timezone.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
  * ATTENTION: An "eval-source-map" devtool has been used.
  * This devtool is neither made for production nor for readable output files.
@@ -6,7 +7,7 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-(function() {
+(() => {
 var exports = {};
 exports.id = "pages/api/timezone";
 exports.ids = ["pages/api/timezone"];
@@ -16,10 +17,9 @@ exports.modules = {
 /*!*******************************!*\
   !*** ./pages/api/timezone.js ***!
   \*******************************/
-/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\nconst timezone = (req, res) => res.status(200).json({\n  ready: true,\n  time: '12:34',\n  location: 'London'\n});\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (timezone);//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90ZXN0My8uL3BhZ2VzL2FwaS90aW1lem9uZS5qcz9jZTVhIl0sIm5hbWVzIjpbInRpbWV6b25lIiwicmVxIiwicmVzIiwic3RhdHVzIiwianNvbiIsInJlYWR5IiwidGltZSIsImxvY2F0aW9uIl0sIm1hcHBpbmdzIjoiO0FBQ0EsTUFBTUEsUUFBUSxHQUFHLENBQUNDLEdBQUQsRUFBTUMsR0FBTixLQUFjQSxHQUFHLENBQUNDLE1BQUosQ0FBVyxHQUFYLEVBQWdCQyxJQUFoQixDQUFxQjtBQUNsREMsT0FBSyxFQUFFLElBRDJDO0FBRWxEQyxNQUFJLEVBQUUsT0FGNEM7QUFHbERDLFVBQVEsRUFBRTtBQUh3QyxDQUFyQixDQUEvQjs7QUFNQSwrREFBZVAsUUFBZiIsImZpbGUiOiIuL3BhZ2VzL2FwaS90aW1lem9uZS5qcy5qcyIsInNvdXJjZXNDb250ZW50IjpbIlxuY29uc3QgdGltZXpvbmUgPSAocmVxLCByZXMpID0+IHJlcy5zdGF0dXMoMjAwKS5qc29uKHtcbiAgcmVhZHk6IHRydWUsXG4gIHRpbWU6ICcxMjozNCcsXG4gIGxvY2F0aW9uOiAnTG9uZG9uJ1xufSk7XG5cbmV4cG9ydCBkZWZhdWx0IHRpbWV6b25lOyJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./pages/api/timezone.js\n");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst timezone = (req, res) => res.status(200).json({\n  ready: true,\n  time: '12:34',\n  location: 'London'\n});\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (timezone);//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9hcGkvdGltZXpvbmUuanMuanMiLCJtYXBwaW5ncyI6Ijs7OztBQUNBLE1BQU1BLFFBQVEsR0FBRyxDQUFDQyxHQUFELEVBQU1DLEdBQU4sS0FBY0EsR0FBRyxDQUFDQyxNQUFKLENBQVcsR0FBWCxFQUFnQkMsSUFBaEIsQ0FBcUI7QUFDbERDLEVBQUFBLEtBQUssRUFBRSxJQUQyQztBQUVsREMsRUFBQUEsSUFBSSxFQUFFLE9BRjRDO0FBR2xEQyxFQUFBQSxRQUFRLEVBQUU7QUFId0MsQ0FBckIsQ0FBL0I7O0FBTUEsaUVBQWVQLFFBQWYiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90ZXN0My8uL3BhZ2VzL2FwaS90aW1lem9uZS5qcz9jZTVhIl0sInNvdXJjZXNDb250ZW50IjpbIlxuY29uc3QgdGltZXpvbmUgPSAocmVxLCByZXMpID0+IHJlcy5zdGF0dXMoMjAwKS5qc29uKHtcbiAgcmVhZHk6IHRydWUsXG4gIHRpbWU6ICcxMjozNCcsXG4gIGxvY2F0aW9uOiAnTG9uZG9uJ1xufSk7XG5cbmV4cG9ydCBkZWZhdWx0IHRpbWV6b25lOyJdLCJuYW1lcyI6WyJ0aW1lem9uZSIsInJlcSIsInJlcyIsInN0YXR1cyIsImpzb24iLCJyZWFkeSIsInRpbWUiLCJsb2NhdGlvbiJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./pages/api/timezone.js\n");
 
 /***/ })
 
@@ -29,7 +29,7 @@ eval("__webpack_require__.r(__webpack_exports__);\nconst timezone = (req, res) =
 // load runtime
 var __webpack_require__ = require("../../webpack-runtime.js");
 __webpack_require__.C(exports);
-var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
 var __webpack_exports__ = (__webpack_exec__("./pages/api/timezone.js"));
 module.exports = __webpack_exports__;
 
--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,8 @@ import { Provider } from "react-redux";
 import { useStore } from "store";
 import { Main } from "layouts";
 
-import "focus-visible/dist/focus-visible.min.js";
-import "styles/main.scss";
+import 'focus-visible/dist/focus-visible.min.js'
+import 'styles/main.scss'
 
 export default function App({ Component, pageProps }) {
   const store = useStore(pageProps.initialReduxState);
